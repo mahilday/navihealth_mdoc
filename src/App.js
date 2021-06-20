3e6aa3338003776e192ac143c9bdc05221d5913e
mahilday <mathildaimadojiemu@gmail.com>
2021-06-20 22:41:13 +0100

    finished homepage and worked on several components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
-import Nav from './components/nav';
-import Home from './pages/Home';
+import "./App.css";
+import Nav from "./components/nav";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className="sticky top-0 z-50">
-      <Nav />
+    <Router>
+      <div className="App">
+        <div className="sticky top-0 z-50">
+          <Nav />
+        </div>
+        <switch>
+          <Route exact path="/" component={Home} />
+        </switch>
       </div>
-      <Home />
-    </div>
+    </Router>
   );
 }
 
