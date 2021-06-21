18fa350afe4613d1020e01109609b3020ecf300a
mahilday <mathildaimadojiemu@gmail.com>
2021-06-21 16:14:43 +0100

    fixed issues

--- src/components/navModal.js
+++ src/components/navModal.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { NavContext } from "../contexts/navContext";
 import { ArrowCircleRightIcon } from "@heroicons/react/solid";
+import { Link } from "react-router-dom";
 import Accordion from "./Accordion";
 
 const NavModal = () => {
@@ -9,7 +10,7 @@ const NavModal = () => {
     <div className="flex flex-col space-y-5 items-center content-center justify-center">
       <ul>
         <li className="border-t py-3 font-semibold flex">
-          <span className="w-44">Find A Provider</span>{" "}
+          <span className="w-44"><Link to="/provider">Find A Provider</Link></span>
           <ArrowCircleRightIcon className="w-6 text-green-free h-6 ml-3" />
         </li>
         <li className="border-t py-3 font-semibold space-y-3 flex flex-col">
